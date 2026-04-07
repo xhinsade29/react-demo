c31633643c20f64197d8bbabd9773336363ff8cc
xhinsade29 <lopezkristine749@gmail.com>
2026-04-07 11:20:48 +0800

    added initial content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonMenuButton, IonPage, IonTitle,IonButton, IonToolbar } from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -7,17 +6,20 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
+        < IonButton slot = "start" >
+                <IonMenuButton> </IonMenuButton>
+                </IonButton>
+                <IonTitle>Home</IonTitle>
+          </IonToolbar>        
         </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Home</IonTitle>            
+            </IonToolbar>
+          </IonHeader>
+        </IonContent>
+  
     </IonPage>
   );
 };
