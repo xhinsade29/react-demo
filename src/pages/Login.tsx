98284aec7d90c3441e74c8409799898325f79f2d
xhinsade29 <lopezkristine749@gmail.com>
2026-04-07 11:51:08 +0800

    added the login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,32 @@
+import { IonPage, IonHeader, IonContent,  IonRouterOutlet,   IonButton, IonMenuButton, useIonRouter  } from "@ionic/react";
+import {  } from "react-router";
+
+
+const Login:React.FC = () => {
+
+    const navigation = useIonRouter();
+
+    const doLogin = () => {
+        
+        navigation.push('/app',  "forward", "replace");
+        
+    };
+    return(
+        <IonPage>
+           <IonHeader>
+            <IonRouterOutlet>
+                <IonButton slot="start">               
+                    <IonMenuButton></IonMenuButton>
+                </IonButton>
+            </IonRouterOutlet>
+           </IonHeader>
+           <IonContent fullscreen>
+            <IonButton expand="full" color="primary" onClick={() => doLogin()}></IonButton>
+            <h1>Login</h1>
+           </IonContent>
+        </IonPage>
+           
+    );
+}
+
+export default Login;
